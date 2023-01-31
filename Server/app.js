78ad5737875b45a0ef22f65b7e5e30c99d749633
Pr3c10us <owo.pre.eno@gmail.com>
2023-01-31 08:13:48 +0100

    changed headrers

--- Server/app.js
+++ Server/app.js
@@ -15,6 +15,14 @@ const app = express();
 // middleware
 app.use(express.json());
 // app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 // Importing the routes
